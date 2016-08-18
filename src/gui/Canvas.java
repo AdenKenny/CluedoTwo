7d65c74525281b8b64a6d104da6ff7e23db894a4
Aden <adenkenny@gmail.com>
2016-08-18 20:51:22 +1200

    Images now exist.

--- src/gui/Canvas.java
+++ src/gui/Canvas.java
@@ -8,23 +8,33 @@ import java.io.IOException;
 import javax.imageio.ImageIO;
 import javax.swing.JPanel;
 
-import cluedo.Board;
-
 public class Canvas extends JPanel {
 
 	private Image boardImage;
+	
 	private Image candlestick;
+	private Image revolver;
+	private Image dagger;
+	private Image pipe;
+	private Image spanner;
+	private Image rope;
 
 	public Canvas() {
 		super();
 		this.boardImage = loadImage("board.png");
 		this.candlestick = loadImage("candlestick.png");
+		this.revolver = loadImage("revolver.png");
+		this.dagger = loadImage("dagger.png");
+		this.pipe = loadImage("pipe.png");
+		this.spanner = loadImage("spanner.png");
+		this.rope = loadImage("rope.png");
 	}
 
 	public void paint(Graphics g) {
 		super.paint(g);
 		g.drawImage(this.boardImage, 0, 0, 800, 760, null, null);
 		g.drawImage(this.candlestick, 80, 60, null, null);
+		g.drawImage(this.revolver, 140, 60, null, null);
 	}
 
 	public static Image loadImage(String filename) {
