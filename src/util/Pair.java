50cc91ade61e1ff77f5dfe21a1de328782bb4780
Aden <adenkenny@gmail.com>
2016-08-26 10:22:34 +1200

    More pair functionality

--- src/util/Pair.java
+++ src/util/Pair.java
@@ -27,4 +27,44 @@ public class Pair<E, T> {
 		return this.value2;
 	}
 
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((this.value1 == null) ? 0 : this.value1.hashCode());
+		result = prime * result + ((this.value2 == null) ? 0 : this.value2.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (obj == null) {
+			return false;
+		}
+		if (getClass() != obj.getClass()) {
+			return false;
+		}
+		Pair other = (Pair) obj;
+		if (this.value1 == null) {
+			if (other.value1 != null) {
+				return false;
+			}
+		} else if (!this.value1.equals(other.value1)) {
+			return false;
+		}
+		if (this.value2 == null) {
+			if (other.value2 != null) {
+				return false;
+			}
+		} else if (!this.value2.equals(other.value2)) {
+			return false;
+		}
+		return true;
+	}
+	
+	
+
 }
